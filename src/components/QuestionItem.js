481d41d972f400a4f807e29be5a25dde23c11eba
Douglas Gatimu <douglasgatimu@gmail.com>
2025-06-02 06:46:13 +0300

    added crud functionalities.

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  const handleDelete = () => {
+    onDeleteQuestion(id);
+  };
+
+  const handleCorrectAnswerChange = (event) => {
+    const newCorrectIndex = parseInt(event.target.value);
+    onUpdateQuestion(id, { correctIndex: newCorrectIndex });
+  };
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +24,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIndex.toString()} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
